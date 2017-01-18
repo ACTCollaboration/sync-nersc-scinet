934a46794d95cc714728f4a26be8e4fc36e62826
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-01-18 17:33:17 -0500

    localscript attempt

--- localScript.sh
+++ localScript.sh
@@ -6,4 +6,4 @@ source sync_common.sh
 NERSC_SYNC_USER="$(trimWhite $(cat nerscUserName))"
 NERSC_DIR="$(trimWhite $(cat nerscPath))"
 
-ssh $NERSC_SYNC_USER@cori.nersc.gov "$NERSC_DIR/syncScript.sh $NERSC_DIR/syncList.sh"
+ssh $NERSC_SYNC_USER@cori.nersc.gov "$NERSC_DIR/syncScript.sh $NERSC_DIR/syncList.txt"
